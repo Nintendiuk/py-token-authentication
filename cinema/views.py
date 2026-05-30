aa472b799c6507ba8a9deadf49f3955eadd28906
Nintendiuk <oleksii.rachynskyi@gmail.com>
2026-05-31 01:04:43 +0300

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.pagination import PageNumberPagination
 from datetime import datetime
 from django.db.models import F, Count
+from rest_framework.permissions import IsAuthenticated
 
 from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
 from cinema.permissions import IsAdminOrIfAuthenticatedReadOnly
@@ -133,7 +134,7 @@ class OrderViewSet(
     )
     serializer_class = OrderSerializer
     pagination_class = OrderPagination
-    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
